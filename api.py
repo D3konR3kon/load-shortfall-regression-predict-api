12131e6d4091f948a99ace3182e07ca07aaf495c
D3konR3kon <103577606+D3konR3kon@users.noreply.github.com>
2024-02-08 04:07:33 +0200

    Update api.py

--- api.py
+++ api.py
@@ -30,7 +30,7 @@ app = Flask(__name__)
 # Load our model into memory.
 # Please update this path to reflect your own trained model.
 static_model = load_model(
-    path_to_model='assets/trained-models/gb_model_final.pkl')
+    path_to_model='assets/trained-models/rf_model_final.pkl')
 
 print ('-'*40)
 print ('Model successfully loaded')
